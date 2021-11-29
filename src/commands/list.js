cdfc5d3de01462c479a6985c0badddb6cf4a0de7
code is sport <95209932+pakobam@users.noreply.github.com>
2021-11-29 16:11:29 +0800

    Update list.js

--- src/commands/list.js
+++ src/commands/list.js
@@ -3,17 +3,18 @@ const Table = require('./../utils/table.js');
 module.exports = {
   name: 'list',
 	async execute(msg, db) {
+if(msg.user.id != process.env.owner_id)return msg.reply({ embeds: [{ description: 'You Don\'t Have Permission To Run This Command', color: '#e83838' }], ephemeral: true });
     const data = await db.list();
     if(!data[0])return await msg.reply({ embeds: [{ description: 'Database Was Empty', color: '#e83838' }], ephemeral: true });
 
   var table = new Table({
-    head: ['Slug', 'URL'],
+    head: ['Slug', 'URL', 'Created By'],
     borders: true
   });
     for(i in data){
-      table.push({
-        [`${data[i]}`]: `${await db.get(data[i])}`,
-      });
+      table.push([
+        `${data[i].split('/')[0]}`, `${await db.get(data[i])}`, `${data[i].split('/')[1]}`
+      ]);
     }
 
 
