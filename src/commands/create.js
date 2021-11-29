0f6f1fd7cae9f287e58ed2f4044d7ff81bb60fb0
code is sport <95209932+pakobam@users.noreply.github.com>
2021-11-29 16:10:10 +0800

    Update create.js

--- src/commands/create.js
+++ src/commands/create.js
@@ -12,13 +12,19 @@ module.exports = {
     if(url.startsWith('https://') || url.startsWith('http://')){
     url = url.substring(url.indexOf("/")+2);
     }
-
+    if(url.includes('ink.is-a.dev'))  return await msg.reply({ embeds: [{ description: "Don't Try To Break Me", color: '#e83838' }], ephemeral: true });
     if(url.includes('/')){
       url = url.split('/')[0];
     }
 
-    const data = await db.get(slug);
-    if(data)return await msg.reply({ embeds: [{ description: 'Slug Exist', color: '#e83838' }], ephemeral: true });
+     var datas = await db.list();
+     var data;
+    for(e of datas) {
+     if(e.startsWith(`${slug}`)) {
+       data = (await db.get(e));
+     }
+      }   
+       if(data)return await msg.reply({ embeds: [{ description: 'Slug Exist', color: '#e83838' }], ephemeral: true });
 
     const schema = Joi.object({
       url: Joi.string().domain({ tlds: true }).required(),
@@ -32,11 +38,11 @@ module.exports = {
     }catch(e){
       return msg.reply({ embeds: [{ description: 'Please Provide A Valid URL & Slug', color: '#e83838' }], ephemeral: true });
     }
-    await db.set(slug, msg.options.getString('url')).then(async () => {
+    await db.set(`${slug}/${msg.user.id}`, msg.options.getString('url')).then(async () => {
       await msg.reply({
         embeds: [{
           title: 'Success !',
-          description: `**Slug:** \`${slug}\`\n**URL:** \`${msg.options.getString('url')}\``,
+          description: `**Slug:** [\`${slug.split('/')[0]}\`](https://ink.is-a.dev/${slug.split('/')[0]})\n**URL:** \`${msg.options.getString('url')}\``,
           color: '#03fc2c'
         }]
       })
